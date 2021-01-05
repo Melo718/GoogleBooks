64c6f5b0de24fbcfc9803ea1021b27f0dc0b5218
camilo alvarez <camiloalvarez88@gmail.com>
2021-01-05 11:00:02 -0500

    my commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Google Books Search
+
+
+## Description
+This application uses Node, Express and MongoDB so that users can save books to review or save later.
+
+- MongoDB
+- Express
+- React
+- Node
---
+++ client/src/components/Homepage/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+function Homepage(props){
+    return (
+    
+        <div className="container">
+            <nav className="navbar navbar-expand-lg text-light" class id="Navbar">
+            </nav>
+            <hr></hr><br></br>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="search"><h2>Search for Your Favorite Book</h2></label>
+                    <input className="Input"
+                        onChange={props.handleInputChange}
+                        value={props.search}
+                        name="search"
+                        type="text"
+                        className="form-control"
+                        placeholder="IT"
+                        id="search"/>
+                    <button onClick={props.handleFormSubmit} className="btn btn-dark mt-3">
+                        Search
+                    </button>
+                </div>
+            </form>
+        </div>
+    
+    );
+}
+
+
+
+export default Homepage;
